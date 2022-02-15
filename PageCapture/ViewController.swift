92e08b1f5fa1afa59eba1fd0ce9951dcecac3680
Suleman Ali <suleman.ali@safarifoneict.com>
2022-02-15 23:02:14 +0500

    Initial Commit

---
+++ PageCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageCapture
+//
+//  Created by safarifone on 15/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
